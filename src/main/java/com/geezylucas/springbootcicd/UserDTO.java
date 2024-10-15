45beb1347b6f81485f142279923583adc2889152
Gerardo Lucas Cruz <glucas@compartamos.com>
2024-10-15 12:26:26 -0600

    feat: add firestore

---
+++ src/main/java/com/geezylucas/springbootcicd/UserDTO.java
@@ -0,0 +1,7 @@
+package com.geezylucas.springbootcicd;
+
+public record UserDTO(String username,
+                      String firstName,
+                      String lastName,
+                      int age) {
+}
